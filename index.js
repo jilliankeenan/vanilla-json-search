612977a0d7f227786f3dfe1c7b96578ee368c2bf
Jillian Keenan <jillian.keenan@puppet.com>
2019-11-18 22:05:29 +0000

    adding more examples

--- index.js
+++ index.js
@@ -1,76 +1,85 @@
 const dataObj = {
   firstname: ["Jillian", "Kez", "Michael", "Sarah"],
   surname: "Keenan",
-  fileType: [
+  corrupted: [
     {
-      corrupted: [
-        {
-          filename: "UNKNOWN",
-          filesize: 1,
-          content: "SCRAMBLED",
-          searchableKeywords: ["margaret", "not feeling well", "dinner"]
-        },
-        {
-          filename: "UNKNOWN",
-          filesize: 2,
-          content: "SCRAMBLED",
-          searchableKeywords: ["Lucas", "night time", "journal"]
-        },
-        {
-          filename: "UNKNOW",
-          filesize: 3,
-          content: "SCRAMBLED",
-          searchableKeywords: ["Jonny", "Diary", "late", "night"]
-        }
-      ]
+      filename: "UNKNOWN",
+      filesize: 1,
+      content: "SCRAMBLED",
+      searchableKeywords: ["margaret", "not feeling well", "dinner"]
     },
     {
-      form: [
-        {
-          passwordProtected: false,
-          filename: "margarets mystery",
-          filesize: 1,
-          content:
-            "I've had a bit of an iffy stomach but I'm feeling a little better, must have been something I've eaten",
-          searchableKeywords: ["margaret", "not feeling well", "dinner"]
-        },
-        {
-          passwordProtected: false,
-          filename: "Lucas Journal",
-          filesize: 2,
-          content:
-            "Late on night judy got out of bed and I wasn't sure what she was doing but it was something",
-          searchableKeywords: ["Lucas", "night time", "journal"]
-        },
-        {
-          passwordProtected: true,
-          filename: "Jonnys Diary",
-          filesize: 3,
-          content:
-            "Late on night judy got out of bed and I wasn't sure what she was doing but it was something",
-          searchableKeywords: ["Jonny", "Diary", "late", "night"]
-        }
-      ]
+      filename: "UNKNOWN",
+      filesize: 2,
+      content: "SCRAMBLED",
+      searchableKeywords: ["Lucas", "night time", "journal"]
+    },
+    {
+      filename: "UNKNOW",
+      filesize: 3,
+      content: "SCRAMBLED",
+      searchableKeywords: ["Jonny", "Diary", "late", "night"]
+    }
+  ],
+  form: [
+    {
+      passwordProtected: false,
+      filename: "margarets mystery",
+      filesize: 1,
+      content:
+        "I've had a bit of an iffy stomach but I'm feeling a little better, must have been something I've eaten",
+      searchableKeywords: ["margaret", "not feeling well", "dinner"]
+    },
+    {
+      passwordProtected: false,
+      filename: "Lucas Journal",
+      filesize: 2,
+      content:
+        "Late on night judy got out of bed and I wasn't sure what she was doing but it was something",
+      searchableKeywords: ["Lucas", "night time", "journal"]
+    },
+    {
+      passwordProtected: true,
+      filename: "Jonnys Diary",
+      filesize: 3,
+      content:
+        "Late on night judy got out of bed and I wasn't sure what she was doing but it was something",
+      searchableKeywords: ["Jonny", "Diary", "late", "night"]
     }
   ]
 };
 
-const thisIsACheck = jsonObj => {
+const thisIsAnExample = jsonObj => {
   // In JSON world this is how we target things within the object.
   console.log(jsonObj.firstname);
-
-  //   const check = jsonObj.map(check => check.firstname)
   return;
 };
 
-const searchObjectByInput = (jsonObj, input) => {
+// How to search specifically for an input value 
+const searchObjectByName = (jsonObj, input) => {
   console.log(jsonObj.firstname.filter(firstName => firstName === input));
 };
 
-thisIsACheck(dataObj);
+// How to search an array and return a value based on whether it was true or false
+const searchObjectBySearchableKeyword = (jsonObj, input) => {
+  jsonObj.corrupted.map(element => {
+    if (element.searchableKeywords.includes(input)) {
+      // Match
+      return console.log("we have a match");
+    }
+    // No match
+    return;
+  });
+};
 
 const search = () => {
-input = document.getElementById("myInput").value;
-document.getElementById("clickMe").onclick = searchObjectByInput(dataObj, input);
-}
+  input = document.getElementById("myInput").value;
+  document.getElementById("clickMe").onclick = searchObjectBySearchableKeyword(
+    dataObj,
+    input
+  );
+};
 
+// We call the function down here
+searchObjectBySearchableKeyword(dataObj);
+thisIsAnExample(dataObj);
